4516e8194d7bc0cfb9c6035c5c0e2ddda4443bc1
Rakesh Shrestha <dev.rakeshshrestha@gmail.com>
2023-06-07 15:47:06 +0545

    Successfull implement ssr for normal html render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from './pages/Home'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+    {
+    path: "/home",
+    element: <Home/>
+  },
+]);
+
+ReactDOM.hydrateRoot(document.getElementById('root'), <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
